00ce9253ba4488d25fa703cd8a7f1997ed14ad87
Adeolu Michael <victooriaoliver75@gmail.com>
2024-10-03 15:41:42 +0100

    Test address added to the merkle tree using scripts

---
+++ script/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {LockInToken} from "../src/LockInToken.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+contract DeployMerkleAirdrop is Script{
+    LockInToken lockInToken;
+    MerkleAirdrop merkleAirdrop;
+
+    function run(bytes32 root) external returns(LockInToken, MerkleAirdrop) {
+        vm.startBroadcast();
+        lockInToken = new LockInToken();
+        merkleAirdrop = new MerkleAirdrop(root, lockInToken);
+        vm.stopBroadcast();
+        return (lockInToken, merkleAirdrop);
+    }
+}
--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -2,8 +2,27 @@
 
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {LockInToken} from "../src/LockInToken.sol";
+import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
 
-contract MerkleAirdropTest{
+contract MerkleAirdropTest is Test{
+    MerkleAirdrop public merkleAirdrop;
+    LockInToken public lockInToken;
+    DeployMerkleAirdrop public deployer;
 
+    bytes32 public constant ROOT_HASH = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    address tommy;
+    uint256 tommyPrivKey;
+
+
+    function setUp() public {
+        deployer = new DeployMerkleAirdrop();
+        (lockInToken, merkleAirdrop) = deployer.run(ROOT_HASH);
+        (tommy, tommyPrivKey) = makeAddrAndKey("tommy");
+    }
+
+    function testUserCanClaim() public view {
+    }
 }
